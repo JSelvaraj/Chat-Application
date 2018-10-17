d387495689fb5ccf39ac60ea05bff81b1116db9e
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2018-10-17 18:22:19 +0100

    0.1.2 Basic Client Functionality
    Added new Exception
    Added methods for getting Destination address.
    Added Method for connecting to currently set address.

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+import common.*;
 
 public class Client {
 
@@ -7,8 +9,19 @@ public class Client {
     private String destinationAddress;
     private int portNumber;
 
+    static int soTimeout = 10; //milliseconds - TcpClientSimpleNB.java
 
-    public void getPortNumber() {
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+    public Socket getSocket() {
+        return socket;
+    }
+    public void inputPortNumber() {
         System.out.println();
         Scanner kb = new Scanner(System.in);
         int tempPort = 0;
@@ -22,12 +35,33 @@ public class Client {
         }
         portNumber = tempPort;
     }
-    public void getPortNumber(int number) {
+    public void inputPortNumber(int number) throws InvalidPortNumberException {
+        if (number < 1023 || number > 65535) {
+            throw new InvalidPortNumberException();
+        } else {
+            portNumber = number;
+        }
+    }
+    public void inputDestinationAddress() {
+        System.out.println();
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Please enter your destination address/IP: ");
+        destinationAddress = kb.nextLine();
+    }
+    public void inputDestinationAddress(String Address) {
+        destinationAddress = Address;
+    }
+    public void connectSocket() {
+        if (destinationAddress == null || portNumber < 1023 || portNumber > 65535) {
+            throw new IllegalArgumentException("Destination Address and/or port number has not been set.");
+        }
+        try {
+            socket = new Socket(destinationAddress, portNumber);
+            socket.setSoTimeout(soTimeout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     }
-    public void getDestinationAddress() {
-
-    }
-
-
-}

fffd1ceb3d55d35b64f8aeafd29cd1a93a0e9f4c
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2018-11-01 12:49:58 +0000

    Add files via upload
    Final Bugfixes

--- src/ServiceMain.java
+++ src/ServiceMain.java
@@ -3,6 +3,7 @@ import common.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -81,6 +82,7 @@ public class ServiceMain implements Runnable {
 
     private static final String ESCAPE_CHARACTER = "q";
     private static final int HOST_SO_TIMEOUT = 10000;
+    private static final int MAX_CONNECTION_ATTEMPTS = 4;
 
     private Socket socket;
     private ServerSocket hostSocket;
@@ -88,8 +90,8 @@ public class ServiceMain implements Runnable {
     private int portNumber = 51638;
     private String username;
 
-    boolean receiveMessagesThreadFlag = false;
-    boolean sendMessagesThreadFlag = false;
+    private boolean receiveMessagesThreadFlag = false;
+    private boolean sendMessagesThreadFlag = false;
 
     public ServiceMain() {
 
@@ -102,7 +104,7 @@ public class ServiceMain implements Runnable {
     /**
      * Gets a port number from the user. Also does some basic checks to ensure it's acceptable.
      */
-    protected void setPortNumber() {
+    private void setPortNumber() {
         System.out.println();
         Scanner kb = new Scanner(System.in);
         int tempPort = 0;
@@ -137,18 +139,20 @@ public class ServiceMain implements Runnable {
      */
     private void connectSocket() throws ClientHasNotConnectedException, InvalidSocketAddressException {
         int i = 0;
-        try {
-            if (destinationAddress == null || portNumber < 1023 || portNumber > 65535) {
-                throw new InvalidSocketAddressException();
-            }
-            System.out.println("Searching for server...");
-            socket = new Socket(destinationAddress, portNumber);
-            System.out.println("Server found...");
-        } catch (IOException e) {
-            System.out.println("Server not found... Retrying...");
-            i++;
-            if (i == 4) {
-                throw new ClientHasNotConnectedException();
+        while (i < MAX_CONNECTION_ATTEMPTS && socket == null) {
+            try {
+                if (destinationAddress == null || portNumber < 1023 || portNumber > 65535) {
+                    throw new InvalidSocketAddressException();
+                }
+                System.out.println("Searching for server...");
+                socket = new Socket(destinationAddress, portNumber);
+                System.out.println("Server found...");
+            } catch (IOException e) {
+                System.out.println("Server not found... Retrying...");
+                i++;
+                if (i == MAX_CONNECTION_ATTEMPTS) {
+                    throw new ClientHasNotConnectedException();
+                }
             }
         }
     }
@@ -179,34 +183,28 @@ public class ServiceMain implements Runnable {
     private void sendMessages() {
         sendMessagesThreadFlag = true;
         try {
-            if (socket == null) {
-                System.out.println("You have not connected to a host...");
-                System.out.println("Returning to menu...");
-            } else {
-                Scanner kb = new Scanner(System.in);
-                String msg = "";
-                PrintWriter sender = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-                System.out.println("You may now enter your messages...");
-                do  {
-                    msg = kb.nextLine();
-                    if (msg.length() > 0) {
-                        sender.println(username + ": " + msg);
-                        sender.flush();
-                    }
-                } while (!msg.equals(ESCAPE_CHARACTER) && receiveMessagesThreadFlag);
-                if (msg.equals(ESCAPE_CHARACTER) ) {
-                    System.out.println("Escape character detected... closing connection to client");
-                    System.out.println("Returning to menu...");
-                } else {
-                    System.out.println();
+            Scanner kb = new Scanner(System.in);
+            String msg = "";
+            PrintWriter sender = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            System.out.println("You may now enter your messages...");
+            do  {
+                msg = kb.nextLine();
+                if (msg.length() > 0) {
+                    sender.println(username + ": " + msg);
+                    sender.flush();
                 }
+            } while (!msg.equals(ESCAPE_CHARACTER) && receiveMessagesThreadFlag);
+            if (msg.equals(ESCAPE_CHARACTER) ) {
+                System.out.println("Escape character detected... Closing connection to client");
+            } else {
+                System.out.println();
             }
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         sendMessagesThreadFlag = false;
-        System.out.println("flag change sending");
     }
 
     /**
@@ -233,35 +231,31 @@ public class ServiceMain implements Runnable {
             String msg;
             msg = "<>";
             do {
-                while (receiver.ready()) {
-                    msg = receiver.readLine();
-                    if (msg != null && !extractMsg(msg).equals(ESCAPE_CHARACTER)) {
-                        System.out.println(msg);
-                    } else if (msg == null)  {
-                        System.out.println("Destination has terminated connection...");
-                        System.out.println("Returning to menu...");
-                        System.out.println("Press enter to return to menu...");
-                    }
+                msg = receiver.readLine();
+                if (msg != null && !extractMsg(msg).equals(ESCAPE_CHARACTER)) {
+                    System.out.println(msg);
+                } else if (msg == null)  {
+                    throw new IOException();
                 }
-            } while (!extractMsg(msg).equals(ESCAPE_CHARACTER) && sendMessagesThreadFlag);
+            } while (msg != null && !extractMsg(msg).equals(ESCAPE_CHARACTER) && sendMessagesThreadFlag);
             if (extractMsg(msg).equals(ESCAPE_CHARACTER)) {
-                System.out.println("Escape Character Detected....");
                 System.out.println("Other terminal has terminated connection...");
                 System.out.println("Press enter twice to return to menu...");
             }
         } catch (IOException e) {
-            System.out.println("Socket has not been connected...");
+            System.out.println("Connection has been terminated");
         }
         receiveMessagesThreadFlag = false;
     }
 
     public void run() {
-        if (socket != null) {
-            receiveMessages();
-        }
+        receiveMessages();
     }
 
-
+    /**
+     * Simple text UI for getting the users choice of function.
+     * @return choice - a number corresponding to a function offered by this application.
+     */
     private int menu() {
         Scanner kb = new Scanner(System.in);
         int choice = 0;
@@ -300,6 +294,9 @@ public class ServiceMain implements Runnable {
         return choice;
     }
 
+    /**
+     * Getsusername from user.
+     */
     private void setUsername() {
         System.out.print("Please Enter your username: ");
         Scanner kb = new Scanner(System.in);
@@ -309,6 +306,12 @@ public class ServiceMain implements Runnable {
     /**
      * Adapted from https://gist.github.com/CarlEkerot/2693246
      *
+     * This method uses a byte stream to send a file to a receiving user.
+     * It follows a simple protocol:
+     * 1. It sends the length of the filename over the outputstream.
+     * 2. It sends the filename over the outputstream
+     * 3. It sends the length of the file over the outputstream
+     * 4. It sends the file over the filestream.
      *
      */
     private void sendFile() {
@@ -351,37 +354,46 @@ public class ServiceMain implements Runnable {
         }
     }
 
+    /**
+     * This method uses a bytestream to receive a file from another terminal. It first creates a FileOutputStream that
+     * uses the helper function getFileName.
+     * Then it follows the protocol:
+     * 1. Reads a long off the inputstream, representing the length of the file being sent.
+     * 2. Reads the inputstream for the length specified above, creating a new file at $PROGRAM DIRECTORY/src/<filename>
+     *
+     */
     private void receiveFile() {
         try {
-            if (socket == null) {
-                System.out.println("You have not connected to another user...");
-                System.out.println("Returning to the menu...");
-            } else {
-                FileOutputStream fos = new FileOutputStream(getFileName());
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
-                long size = 0;
-                size = dis.readLong();
-
-                byte[] buffer = new byte[4096];
-                int read = 0;
-                int position = 0;
-                int remaining = (int) size - read;
-                while ((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
-                    position += read;
-                    remaining -= read;
-                    fos.write(buffer, 0, read);
-                }
-                dis.close();
-                fos.close();
-                System.out.println("File successfully received...");
-                System.out.println("Program will now return to menu...");
+            FileOutputStream fos = new FileOutputStream(getFileName());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            long size = 0;
+            size = dis.readLong();
+            byte[] buffer = new byte[4096];
+            int read = 0;
+            int position = 0;
+            int remaining = (int) size - read;
+            while ((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+                position += read;
+                remaining -= read;
+                fos.write(buffer, 0, read);
             }
+            dis.close();
+            fos.close();
+            System.out.println("File successfully received...");
+            System.out.println("Program will now return to menu...");
         } catch (IOException e) {
             System.out.println("Connection Lost returning to menu...");
         }
-
     }
 
+    /**
+     * Helper function that:
+     * 1. reads an int from the inputstream, representing the lenght of the filename
+     * 2. Reads that many characters off the inputstream.
+     * 3. Turns the resulting character array into a String.
+     * @return filename - a String containing the filename of the file being sent.
+     * @throws IOException If something interrupts the stream.
+     */
     private String getFileName() throws IOException {
 
         DataInputStream dis = new DataInputStream(socket.getInputStream());
@@ -391,8 +403,20 @@ public class ServiceMain implements Runnable {
         }
         return new String(fileName);
     }
+
+    /**
+     * This method closes all active sockets.
+     *
+     * I currently do not know why, but if there is not a Thread.sleep call, the program will not continue even if both
+     * flags are false.
+     */
     private void closeSocket() {
         while (receiveMessagesThreadFlag || sendMessagesThreadFlag) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+
+            }
         }
         try {
             socket.close();
